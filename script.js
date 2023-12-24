0d99734803094b39d5c0a3c70642a8691e0606ca
John-0liveira <johnreisdeoliveira@gmail.com>
2023-12-24 17:01:21 -0300

    Criando primiras caixas de texto

--- script.js
+++ script.js
@@ -3,10 +3,31 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Código a ser executado quando o conteúdo da página estiver carregado
 
-    // Adiciona um evento de clique ao botão
-    const meuBotao = document.getElementById('meuBotao');
-    meuBotao.addEventListener('click', function () {
-        // Exibe uma mensagem de teste
-        alert('Parabéns! Seu botão está funcionando.');
+    // Adiciona um evento de clique ao botão "Salvar Dados"
+    const salvarDadosBtn = document.getElementById('salvarDadosBtn');
+    salvarDadosBtn.addEventListener('click', function () {
+        // Obtém os valores das caixas de inserção de texto
+        const cnpjValue = document.getElementById('cnpjInput').value;
+        const eventoValue = document.getElementById('eventoInput').value;
+        const dataValue = document.getElementById('dataInput').value;
+        const processoValue = document.getElementById('processoInput').value;
+
+        // Validação simples - verifica se todas as caixas estão preenchidas
+        if (!cnpjValue || !eventoValue || !dataValue || !processoValue) {
+            alert('Por favor, preencha todos os campos antes de salvar.');
+            return;
+        }
+
+        // Cria uma string com os dados formatados
+        const dadosFormatados = `${cnpjValue}, ${eventoValue}, ${dataValue}, ${processoValue}`;
+
+        // Abre uma caixa de diálogo para salvar os dados em um arquivo TXT
+        const blob = new Blob([dadosFormatados], { type: 'text/plain' });
+        const a = document.createElement('a');
+        a.href = URL.createObjectURL(blob);
+        a.download = 'dados.txt';
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
     });
 });
