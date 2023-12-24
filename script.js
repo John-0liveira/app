55c7a4c4a29eff12876a5f17eeaff7caa71a27da
John-0liveira <johnreisdeoliveira@gmail.com>
2023-12-24 18:35:00 -0300

    Campo cnpj corrigido no javascript

--- script.js
+++ script.js
@@ -21,8 +21,14 @@ document.addEventListener('DOMContentLoaded', function () {
         // Cria uma string com os dados formatados
         const dadosFormatados = `${cnpjValue}, ${eventoValue}, ${dataValue}, ${processoValue}`;
 
+        // Converte a string para uma matriz de bytes usando windows-1252
+        const byteArray = new Uint8Array(dadosFormatados.length);
+        for (let i = 0; i < dadosFormatados.length; i++) {
+            byteArray[i] = dadosFormatados.charCodeAt(i) & 0xff;
+        }
+
         // Abre uma caixa de diálogo para salvar os dados em um arquivo TXT (com codificação windows-1252)
-        const blob = new Blob([dadosFormatados], { type: 'text/plain;charset=windows-1252' });
+        const blob = new Blob([byteArray], { type: 'text/plain' });
         const a = document.createElement('a');
         a.href = URL.createObjectURL(blob);
         a.download = 'dados.txt';
@@ -30,4 +36,27 @@ document.addEventListener('DOMContentLoaded', function () {
         a.click();
         document.body.removeChild(a);
     });
+
+// Adiciona um evento de entrada à caixa de CNPJ para formatar automaticamente
+const cnpjInput = document.getElementById('cnpjInput');
+cnpjInput.addEventListener('input', formatarCnpj);
+
+// Função para formatar o CNPJ
+function formatarCnpj() {
+    let valor = cnpjInput.value.replace(/\D/g, ''); // Remove caracteres não numéricos
+    const tamanho = valor.length;
+
+    if (tamanho >= 2 && tamanho < 5) {
+        valor = `${valor.substr(0, 2)}.${valor.substr(2)}`;
+    } else if (tamanho >= 5 && tamanho < 8) {
+        valor = `${valor.substr(0, 2)}.${valor.substr(2, 3)}.${valor.substr(5)}`;
+    } else if (tamanho >= 8 && tamanho < 12) {
+        valor = `${valor.substr(0, 2)}.${valor.substr(2, 3)}.${valor.substr(5, 3)}/${valor.substr(8)}`;
+    } else if (tamanho >= 12) {
+        valor = `${valor.substr(0, 2)}.${valor.substr(2, 3)}.${valor.substr(5, 3)}/${valor.substr(8, 4)}-${valor.substr(12)}`;
+    }
+
+    cnpjInput.value = valor;
+}
+
 });
