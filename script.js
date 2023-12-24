1094dd321f0a2712f95afeec2463a8c1a2a408f1
John-0liveira <johnreisdeoliveira@gmail.com>
2023-12-24 16:57:08 -0300

    Arquivos iniciais de teste.

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Código a ser executado quando o conteúdo da página estiver carregado
+
+    // Adiciona um evento de clique ao botão
+    const meuBotao = document.getElementById('meuBotao');
+    meuBotao.addEventListener('click', function () {
+        // Exibe uma mensagem de teste
+        alert('Parabéns! Seu botão está funcionando.');
+    });
+});
