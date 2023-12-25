df0a7ee63af9c1007f5250fc1c292123c1bcb5a1
John-0liveira <johnreisdeoliveira@gmail.com>
2023-12-25 00:09:45 -0300

    Campos formatados corretamente para novas linhas.

--- script.js
+++ script.js
@@ -43,23 +43,21 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     // Adiciona um evento de entrada à caixa de CNPJ para formatar automaticamente
-    const cnpjInput = document.getElementById('cnpjInput');
     cnpjInput.addEventListener('input', function () {
         this.value = this.value.replace(/\D/g, '').substr(0, 14);
-        formatarCnpj();
+        formatarCnpj(this); // Passa 'this' como argumento
     });
 
     // Adiciona um evento de entrada à caixa de Data para formatar automaticamente
-    const dataInput = document.getElementById('dataInput');
     dataInput.addEventListener('input', function () {
         this.value = this.value.replace(/\D/g, '').substr(0, 8);
-        formatarData();
+        formatarData(this); // Passa 'this' como argumento
     });
 
     // Adiciona um evento de entrada à caixa de Evento para limitar o número de caracteres
     const eventoInput = document.getElementById('eventoInput');
     eventoInput.addEventListener('input', function () {
-        this.value = this.value.substr(0, 3);
+        this.value = this.value.replace(/\D/g, '').substr(0, 3); // Limita a entrada a 3 dígitos numéricos
     });
 
     // Adiciona um evento de entrada à caixa de Processo para limitar o número de caracteres
@@ -77,10 +75,10 @@ document.addEventListener('DOMContentLoaded', function () {
     removerLinhaBtn.addEventListener('click', removerLinha);
 
     // Função para formatar o CNPJ
-    function formatarCnpj() {
-        let valor = cnpjInput.value.replace(/\D/g, ''); // Remove caracteres não numéricos
+    function formatarCnpj(input) {
+        let valor = input.value.replace(/\D/g, ''); // Remove caracteres não numéricos
         const tamanho = valor.length;
-
+    
         if (tamanho >= 2 && tamanho < 5) {
             valor = `${valor.substr(0, 2)}.${valor.substr(2)}`;
         } else if (tamanho >= 5 && tamanho < 8) {
@@ -90,52 +88,69 @@ document.addEventListener('DOMContentLoaded', function () {
         } else if (tamanho >= 12) {
             valor = `${valor.substr(0, 2)}.${valor.substr(2, 3)}.${valor.substr(5, 3)}/${valor.substr(8, 4)}-${valor.substr(12)}`;
         }
-
-        cnpjInput.value = valor;
+    
+        input.value = valor; // Atualiza o valor do campo de entrada específico
     }
 
-    // Função para formatar a Data
-    function formatarData() {
-        let valor = dataInput.value.replace(/\D/g, ''); // Remove caracteres não numéricos
+    function formatarData(input) {
+        let valor = input.value.replace(/\D/g, ''); // Remove caracteres não numéricos
         const tamanho = valor.length;
-
+    
         if (tamanho > 2) {
             valor = `${valor.substr(0, 2)}/${valor.substr(2)}`;
         }
-
+    
         if (tamanho > 4) {
             valor = `${valor.substr(0, 5)}/${valor.substr(5)}`;
         }
-
-        dataInput.value = valor;
+    
+        input.value = valor; // Atualiza o valor do campo de entrada específico
     }
 
     // Função para adicionar uma nova linha
-function adicionarLinha() {
-    const container = document.getElementById('linhas-container');
-    let novaLinha = document.createElement('div');
-    novaLinha.className = 'linha';
-    novaLinha.innerHTML = `
-        <div class="numero-linha">${++numeroLinha}</div>
-        <div class="caixa">
-            <label for="cnpjInput${numeroLinha}">CNPJ:</label>
-            <input type="text" id="cnpjInput${numeroLinha}" placeholder="00.000.000/0000-00">
-        </div>
-        <div class="caixa">
-            <label for="eventoInput${numeroLinha}">Código do Evento:</label>
-            <input type="text" id="eventoInput${numeroLinha}">
-        </div>
-        <div class="caixa">
-            <label for="dataInput${numeroLinha}">Data do Fato Motivador:</label>
-            <input type="text" id="dataInput${numeroLinha}" placeholder="dd/mm/aaaa">
-        </div>
-        <div class="caixa">
-            <label for="processoInput${numeroLinha}">Número do Processo:</label>
-            <input type="text" id="processoInput${numeroLinha}">
-        </div>
-    `;
-    container.appendChild(novaLinha);
-}
+    function adicionarLinha() {
+        const container = document.getElementById('linhas-container');
+        let novaLinha = document.createElement('div');
+        novaLinha.className = 'linha';
+        const novaLinhaNumero = ++numeroLinha;
+        novaLinha.innerHTML = `
+            <div class="numero-linha">${novaLinhaNumero}</div>
+            <div class="caixa">
+                <label for="cnpjInput${novaLinhaNumero}">CNPJ:</label>
+                <input type="text" id="cnpjInput${novaLinhaNumero}" placeholder="00.000.000/0000-00">
+            </div>
+            <div class="caixa">
+                <label for="eventoInput${novaLinhaNumero}">Código do Evento:</label>
+                <input type="text" id="eventoInput${novaLinhaNumero}">
+            </div>
+            <div class="caixa">
+                <label for="dataInput${novaLinhaNumero}">Data do Fato Motivador:</label>
+                <input type="text" id="dataInput${novaLinhaNumero}" placeholder="dd/mm/aaaa">
+            </div>
+            <div class="caixa">
+                <label for="processoInput${novaLinhaNumero}">Número do Processo:</label>
+                <input type="text" id="processoInput${novaLinhaNumero}">
+            </div>
+        `;
+        container.appendChild(novaLinha);
+        
+        // Adiciona eventos de formatação aos novos campos
+        document.getElementById(`cnpjInput${novaLinhaNumero}`).addEventListener('input', function() {
+            this.value = this.value.replace(/\D/g, '').substr(0, 14);
+            formatarCnpj(this);
+        });
+        document.getElementById(`dataInput${novaLinhaNumero}`).addEventListener('input', function() {
+            this.value = this.value.replace(/\D/g, '').substr(0, 8);
+            formatarData(this);
+        });
+        document.getElementById(`eventoInput${novaLinhaNumero}`).addEventListener('input', function() {
+            this.value = this.value.replace(/\D/g, '').substr(0, 3); // Limita a entrada a 3 dígitos numéricos
+        });
+        document.getElementById(`processoInput${novaLinhaNumero}`).addEventListener('input', function() {
+            this.value = this.value.substr(0, 25);
+        });
+    }
+
     function removerLinha() {
         const container = document.getElementById('linhas-container');
         if (container.children.length > 1) {
